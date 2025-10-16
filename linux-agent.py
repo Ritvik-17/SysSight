64cb19374171cb3822ebdfe1c5808ab52e2d22fa
RitTheDev <73381025+Ritvik-17@users.noreply.github.com>
2025-10-16 21:55:24 +0530

    first commit

---
+++ linux-agent.py
@@ -0,0 +1,21 @@
+import psutil
+import time
+import json
+import platform
+
+def get_system_info():
+    info = {
+        "hostname": platform.node(),
+        "os": platform.system(),
+        "os_version": platform.version(),
+        "cpu_percent": psutil.cpu_percent(interval=1),
+        "memory_percent": psutil.virtual_memory().percent,
+        "disk_percent": psutil.disk_usage('/').percent
+    }
+    return info
+
+if __name__ == "__main__":
+    while True:
+        data = get_system_info()
+        print(json.dumps(data, indent=2))
+        time.sleep(5)
