09a476635b42077e79d710b2955e5334828d9b98
RitTheDev <73381025+Ritvik-17@users.noreply.github.com>
2025-10-17 21:46:31 +0530

    Add multiple agents

--- Central Server/app.js
+++ Central Server/app.js
@@ -6,13 +6,11 @@ var fs = require("fs");
 var app = express();
 var PORT = process.env.PORT || 4000;
 app.use(express.json());
-
-var DATA_FILE = path.join(__dirname, 'agent-data.json');
-
+var DataFile = path.join(__dirname, 'agent-data.json');
 function readAgentData() {
     try {
-        if (fs.existsSync(DATA_FILE)) {
-            var raw = fs.readFileSync(DATA_FILE, 'utf-8');
+        if (fs.existsSync(DataFile)) {
+            var raw = fs.readFileSync(DataFile, 'utf-8');
             return JSON.parse(raw);
         }
         return [];
@@ -22,43 +20,33 @@ function readAgentData() {
         return [];
     }
 }
-
 function saveAgentData(data) {
     try {
-        fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2), 'utf-8');
+        fs.writeFileSync(DataFile, JSON.stringify(data, null, 2), 'utf-8');
     }
     catch (err) {
         console.error('Failed to write data file:', err);
     }
 }
-
 app.get('/', function (req, res) {
     res.send('Server is running!');
 });
-
 app.get('/dashboard', function (req, res) {
     res.sendFile(path.join(__dirname, 'public/dashboard.html'));
 });
-
 app.post('/agent-data', function (req, res) {
     var _a = req.body, hostname = _a.hostname, cpuUsage = _a.cpuUsage, memoryUsage = _a.memoryUsage;
     if (!hostname || cpuUsage === undefined || memoryUsage === undefined) {
         return res.status(400).send('Invalid data');
     }
     var timestamp = new Date().toISOString();
-
     var agentEntry = { hostname: hostname, cpuUsage: cpuUsage, memoryUsage: memoryUsage, createdAt: timestamp };
-
     console.log('📥 Received data from agent:', agentEntry);
-
     var allData = readAgentData();
-
     allData.push(agentEntry);
-
     saveAgentData(allData);
     res.status(200).send('Data received, logged, and stored');
 });
-
 app.get('/agent-data', function (req, res) {
     var allData = readAgentData();
     res.json(allData);
