35ad6b7078c80edfbabcd0544abd368843cd861b
RitTheDev <73381025+Ritvik-17@users.noreply.github.com>
2025-10-17 12:51:36 +0530

    Add dashboard

--- Central Server/app.js
+++ Central Server/app.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 4000;
 app.use(express.json());
@@ -8,11 +9,17 @@ app.use(express.json());
 app.get('/', function (req, res) {
     res.send('Server is running!');
 });
+app.get('/dashboard', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/dashboard.html'));
+});
 // Route to receive agent data
 app.post('/agent-data', function (req, res) {
     var _a = req.body, hostname = _a.hostname, cpuUsage = _a.cpuUsage, memoryUsage = _a.memoryUsage;
     console.log('Received data from agent:', { hostname: hostname, cpuUsage: cpuUsage, memoryUsage: memoryUsage });
     res.status(200).send('Data received');
+});
+app.get('/agent-data', function (req, res) {
+    res.status(200).send('Hello, i will send agent data later');
 });
 app.listen(PORT, function () {
     console.log("\uD83D\uDE80 Server running on port ".concat(PORT));
--- Central Server/app.ts
+++ Central Server/app.ts
@@ -1,4 +1,5 @@
 import express = require('express');
+import path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -10,11 +11,19 @@ app.get('/', (req: any, res: any) => {
   res.send('Server is running!');
 });
 
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/dashboard.html'));
+});
+
 // Route to receive agent data
 app.post('/agent-data', (req: any, res: any) => {
   const { hostname, cpuUsage, memoryUsage } = req.body;
   console.log('Received data from agent:', { hostname, cpuUsage, memoryUsage });
   res.status(200).send('Data received');
+});
+
+app.get('/agent-data', (req: any, res: any) => {
+  res.status(200).send('Hello, i will send agent data later');
 });
 
 app.listen(PORT, () => {
