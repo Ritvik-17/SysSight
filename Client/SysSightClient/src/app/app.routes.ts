2b9fd5ddc17e272a94ba8632afd9d000a3354ee7
RitTheDev <73381025+Ritvik-17@users.noreply.github.com>
2025-10-24 23:54:25 +0530

    Add view processes feature

--- Client/SysSightClient/src/app/app.routes.ts
+++ Client/SysSightClient/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashBoard } from './dash-board/dash-board';
 import { AgentDashboard } from './agent-dashboard/agent-dashboard';
+import { Processes } from './agent-dashboard/processes/processes';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // default redirect
     { path: 'dashboard', component: DashBoard },
-    { path: 'agent/:agentId', component: AgentDashboard}
+    { path: 'agent/:agentId', component: AgentDashboard},
+        { path: 'process/:agentId', component: Processes}
 ];
